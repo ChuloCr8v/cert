c5f966c1a711096ffca8cfff7f0ab8eacf611aff
chulocr8v <nkematu5@gmail.com>
2023-12-18 10:06:26 +0100

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,135 @@
-import Image from 'next/image'
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import html2canvas from "html2canvas";
+import { Modal } from "antd";
 
 export default function Home() {
+  const [awardee, setAwardee] = useState("");
+  const [days, setDays] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleDownload = () => {
+    const element = document.getElementById("downloadableDiv");
+
+    html2canvas(element, {
+      scale: 5,
+    }).then((canvas) => {
+      // Convert canvas to data URL
+      const dataURL = canvas.toDataURL();
+
+      // Create a link element
+      const link = document.createElement("a");
+
+      // Set the download attribute and convert data URL to Blob
+      link.download = "downloaded_image.png";
+      link.href = dataURL;
+
+      // Trigger the download
+      link.click();
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <main className="flex flex-col items-center justify-center py-12 overflow-x-hidden">
+      <h2 className="font-bold text-4xl text-center ">
+        StableNet Certificate Generator
+      </h2>
+      <div className="max-w-7xl w-full xl:grid grid-cols-2 xl:gap-96 justify-center px-2 flex flex-col items-center -mt-32">
+        <div
+          className="cert-bg w-[1056px] h-[750px] flex flex-col items-center gap-8 py-16 scale-[.35] md:scale-[.5]"
+          id="downloadableDiv"
+        >
+          <div className="flex flex-col items-center gap-4">
+            <Image src="/assets/badge.png" alt="badge" height={60} width={60} />
+            <p className="text-5xl uppercase text-center leading-snug">
+              Certificate <span className="block text-3xl">of completion</span>
+            </p>
+          </div>
+          <div className="text-center space-y-4">
+            <p className="text-4xl font-bold capitalize px-2">
+              {awardee ? awardee : "Awardee Name"}
+            </p>
+            <p className="text-2xl">
+              has successfully completed the {days ? days : 0}-day Training
+              Combined and is now a
+            </p>
+          </div>
+          <div className="">
+            <p className="text-5xl font-bold text-[#1578af] text-center leading-[1.2]">
+              STABLENET<span className="">®</span>
+              <span className="block">CERTIFIED ENGINEER</span>
+            </p>
+          </div>
+          <div className="relative flex flex-col justify-center items-center">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/assets/sig.png"
+              alt="signature"
+              height={200}
+              width={200}
+              className="absolute -mt-12"
             />
-          </a>
+            <p className="text-2xl uppercase border-t border-black px-4 pt-2 mt-12">
+              signature
+            </p>
+          </div>{" "}
+          <div className="">
+            <Image
+              src="/assets/logo.png"
+              alt="badge"
+              height={200}
+              width={200}
+            />
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Modal
+          title="Basic Modal"
+          open={isOpen}
+          onCancel={() => setIsOpen(false)}
+          onOk={handleDownload}
+          okText={"Download"}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <div className="grid gap-4 mt-4">
+            <div className="w-full grid gap-2">
+              <label htmlFor="">Awardee Name</label>
+              <input
+                type="text"
+                placeholder="Add name of Awardee"
+                name=""
+                id=""
+                className="p-2 rounded w-full border outline-none hover:border-blue-500 duration-300"
+                onChange={(e) => setAwardee(e.target.value)}
+              />
+            </div>
+            <div className="grid gap-2">
+              <label htmlFor="">Training Duration</label>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+              <input
+                type="number"
+                placeholder="Add days of training"
+                name=""
+                id=""
+                className="p-2 rounded w-full border outline-none hover:border-blue-500 duration-300"
+                onChange={(e) => setDays(e.target.value)}
+              />
+            </div>
+            {/* <button
+              onClick={handleDownload}
+              className="p-2 rounded w-full border outline-none bg-blue-500 hover:border-blue-500 duration-300 text-white uppercase font-bold mt-4"
+            >
+              download
+            </button> */}
+          </div>{" "}
+        </Modal>
+        <button
+          onClick={() => setIsOpen(true)}
+          className="p-2 rounded w-full border outline-none bg-blue-500 hover:border-blue-500 duration-300 text-white uppercase font-bold -mt-48"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Add Awardee
+        </button>
       </div>
     </main>
-  )
+  );
 }
